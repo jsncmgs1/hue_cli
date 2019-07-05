d6d076110f8fb21829124f3520b729805fcd042c
Jason Cummings <jasoncummings86@gmail.com>
2019-07-05 17:32:54 -0400

    Init

---
+++ hue.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "net/http"
+  "io/ioutil"
+  "gopkg.in/alecthomas/kingpin.v2"
+  "github.com/imroc/req"
+  "bytes"
+  "regexp"
+  "utils"
+)
+
+var url string = os.Getenv("HUE_URL")
+var kitchenLights = [4]int{1,2,4}
+
+type LightCommand struct {
+  All bool
+}
+
+func (light *LightCommand) run(c *kingpin.ParseContext) error {
+  url := fmt.Sprintf("%slights", url)
+  resp, _ := http.Get(url)
+  defer resp.Body.Close()
+  bodyBytes, _ := ioutil.ReadAll(resp.Body)
+  utils.PrettyPrint(bodyBytes)
+  return nil
+}
+
+func configureLightCommand(app *kingpin.Application) {
+  c:= &LightCommand{}
+  app.Command("lights", "Get light info").Action(c.run)
+}
+
+type KitchenLightCommand struct {
+  All bool
+}
+
+func parseSubCommandBool(subc *kingpin.ParseContext, expectedValue string) bool {
+  re := regexp.MustCompile(`\s`)
+  arr := re.Split(subc.String(), 2)
+
+  return arr[1] == expectedValue
+}
+
+func (lights *KitchenLightCommand) run(c *kingpin.ParseContext) error {
+  state := parseSubCommandBool(c, "on")
+
+  for i:= 0; i < len(kitchenLights); i++ {
+    url:= fmt.Sprintf("%slights/%d/state", url, kitchenLights[i])
+    var jsonStr = []byte(fmt.Sprintf(`{"on":%t}`, state))
+    req.Put(url, bytes.NewBuffer(jsonStr))
+  }
+
+  return nil
+}
+
+func configureKitchenLightCommand(app *kingpin.Application) {
+  c := &KitchenLightCommand{}
+  kitchenCommand  := app.Command("kitchen", "Control Kitchen Lights")
+  kitchenCommand.Command("off", "turn kitchen lights off").Action(c.run)
+  kitchenCommand.Command("on", "turn kitchen lights on").Action(c.run)
+}
+
+func configureCommands(app *kingpin.Application) {
+  configureLightCommand(app)
+  configureKitchenLightCommand(app)
+}
+
+func main() {
+  app := kingpin.New("hue", "Phillips Hue CLI")
+  configureCommands(app)
+  kingpin.MustParse(app.Parse(os.Args[1:]))
+}
