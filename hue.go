57b80b06d88c1a478c14d58ac3c3e4190647c4cb
Jason Cummings <jasoncummings86@gmail.com>
2019-07-07 11:46:55 -0400

    Refactor light on/off

--- hue.go
+++ hue.go
@@ -9,14 +9,12 @@ import (
   "github.com/imroc/req"
   "bytes"
   "utils"
-  "sync"
   "encoding/json"
   "strconv"
   "strings"
 )
 
 var url string = os.Getenv("HUE_URL")
-var kitchenLights = [4]int{1,2,4}
 
 type LightCommand struct {
   All bool
@@ -38,54 +36,42 @@ func configureLightCommand(app *kingpin.Application) {
 
 type RoomLightCommand struct {
   All bool
+  LightGroup *Group
 }
 
 func (lights *RoomLightCommand) run(c *kingpin.ParseContext) error {
-  var wg sync.WaitGroup
-  wg.Add(len(kitchenLights))
   state := os.Args[2] == "on"
-
-  for i:= 0; i < len(kitchenLights); i++ {
-    url:= fmt.Sprintf("%slights/%d/state", url, kitchenLights[i])
-    var jsonStr = []byte(fmt.Sprintf(`{"on":%t}`, state))
-    go func() {
-      defer wg.Done()
-      req.Put(url, bytes.NewBuffer(jsonStr))
-    }()
-  }
-
-  wg.Wait()
+  fmt.Println(lights.LightGroup)
+  url:= fmt.Sprintf("%sgroups/%s/action", url, lights.LightGroup.Id)
+  fmt.Println(url)
+  var jsonStr = []byte(fmt.Sprintf(`{"on":%t}`, state))
+  req.Put(url, bytes.NewBuffer(jsonStr))
   return nil
 }
 
-type Room struct {
-  name string
-  lightIds []string
+type Group struct {
+  Name string
+  Id string
 }
 
-func configureRoomLightCommand(app *kingpin.Application) {
+func configureRoomsLightCommand(app *kingpin.Application) {
   var result map[string]interface{}
 
-  c := &RoomLightCommand{}
   groupsUrl := fmt.Sprintf("%sgroups", url)
 
   resp, _ := http.Get(groupsUrl)
+  defer resp.Body.Close()
+
   bodyBytes, _ := ioutil.ReadAll(resp.Body)
   json.Unmarshal(bodyBytes, &result)
 
   for i := 0; i < len(result); i++ {
-    first:= result[strconv.Itoa(i+1)].(map[string]interface{})
+    c := &RoomLightCommand{}
+    id := strconv.Itoa(i+1)
+    first:= result[id].(map[string]interface{})
     name := strings.ToLower(first["name"].(string))
-    lights := first["lights"].([]interface{})
-    length := len(lights)
-    lightsArr := make([]string, length)
+    c.LightGroup = &Group{Name: name, Id: id}
 
-    for i := 0; i < length; i++ {
-      t, _ := lights[i].(string)
-      lightsArr[i] = t
-    }
-
-    room := &Room{lightIds: lightsArr, name: name}
     on := fmt.Sprintf("Turn %s lights on", name)
     off := fmt.Sprintf("Turn %s lights off", name)
 
@@ -97,7 +83,7 @@ func configureRoomLightCommand(app *kingpin.Application) {
 
 func configureCommands(app *kingpin.Application) {
   configureLightCommand(app)
-  configureRoomLightCommand(app)
+  configureRoomsLightCommand(app)
 }
 
 func main() {
