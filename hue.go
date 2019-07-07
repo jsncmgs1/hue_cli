96d0a3e9ea0cc69f144ba614b1ace13f6a212ae5
Jason Cummings <jasoncummings86@gmail.com>
2019-07-07 13:56:43 -0400

    Use client package

--- hue.go
+++ hue.go
@@ -1,26 +1,21 @@
 package main
 
 import (
-  "fmt"
   "os"
-  "net/http"
+  "fmt"
   "io/ioutil"
   "gopkg.in/alecthomas/kingpin.v2"
-  "github.com/imroc/req"
-  "bytes"
   "utils"
+  httpClient "client"
   "encoding/json"
   "strconv"
   "strings"
 )
 
-
 type LightCommand struct {
-  All bool
 }
 
 type RoomLightCommand struct {
-  All bool
   LightGroup *Group
 }
 
@@ -28,16 +23,6 @@ type Group struct {
   Name string
   Id string
 }
-
-type Client struct {
-}
-
-func(client *Client) Get(url string) (*http.Response, error) {
-  resp, err := http.Get(url)
-  return resp, err
-}
-
-var client = &Client{}
 
 var url string = os.Getenv("HUE_URL")
 
@@ -49,6 +34,12 @@ func getGroupsUrl() string {
   return fmt.Sprintf("%sgroups", url)
 }
 
+func groupsActionUrl(groupId string) string {
+  return fmt.Sprintf("%sgroups/%s/action", url, groupId)
+}
+
+var client = &httpClient.Client{}
+
 func (light *LightCommand) run(c *kingpin.ParseContext) error {
   resp, _ := client.Get(getLightsUrl())
   defer resp.Body.Close()
@@ -57,18 +48,18 @@ func (light *LightCommand) run(c *kingpin.ParseContext) error {
   return nil
 }
 
+func (lights *RoomLightCommand) run(c *kingpin.ParseContext) error {
+  state := os.Args[2] == "on"
+  url := groupsActionUrl(lights.LightGroup.Id)
+  var jsonStr = fmt.Sprintf(`{"on":%t}`, state)
+
+  client.Put(url, jsonStr)
+  return nil
+}
+
 func configureLightCommand(app *kingpin.Application) {
   c:= &LightCommand{}
   app.Command("lights", "Get light info").Action(c.run)
-}
-
-func (lights *RoomLightCommand) run(c *kingpin.ParseContext) error {
-  state := os.Args[2] == "on"
-  url:= fmt.Sprintf("%sgroups/%s/action", url, lights.LightGroup.Id)
-  fmt.Println(url)
-  var jsonStr = []byte(fmt.Sprintf(`{"on":%t}`, state))
-  req.Put(url, bytes.NewBuffer(jsonStr))
-  return nil
 }
 
 func configureRoomsLightCommand(app *kingpin.Application) {
@@ -79,7 +70,6 @@ func configureRoomsLightCommand(app *kingpin.Application) {
   json.Unmarshal(bodyBytes, &result)
 
   for i := 0; i < len(result); i++ {
-    fmt.Println("*******************")
     c := &RoomLightCommand{}
     id := strconv.Itoa(i+1)
     first:= result[id].(map[string]interface{})
